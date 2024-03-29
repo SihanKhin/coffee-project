88070d4a1ee99ff3fbe08e970035eb546c129c45
SihanKhin <sihankhin@gmail.com>
2024-03-29 12:16:50 +0700

    updated the product cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -27,33 +27,41 @@ const Cart = () => {
     dispatch(deleteAllProduct(productId))
   }
   console.log(products)
+  const productQuantity = products.reduce((acc,cur)=>(
+        acc + cur.quantity
+  ),0)
+  const displayQuantity = productQuantity >  1 ?  "ITEMS" : "ITEM" 
   return (
     <main className='flex flex-grow w-full min-h-screen justify-start items-center'>
        <div className='flex flex-col justify-center items-center w-full min-h-screen'>
-           <div className='flex flex-col justify-center items-start w-11/12 min-h-20 md:-ml-7'>
-            <h1 className='text-3xl font-Roboto font-semibold '>Shopping Cart</h1>
+           <div className='flex flex-row justify-between items-start w-11/12 py-10  md:-ml-7'>
+              <h1 className='text-3xl font-Roboto font-semibold '>Shopping Cart</h1>
+              <div className='md:w-80 md:mr-8'><span className='text-2xl font-Roboto font-medium'>{productQuantity} {displayQuantity}</span></div>
             </div>
         <div className='flex flex-col md:flex-row w-full justify-center items-center gap-x-2 pt-4'>
+          
           <div className='flex flex-col justify-start items-start bg-white min-h-96 w-11/12 md:w-9/12 border
               border-gray-300 rounded
-            '>
+            '>              
                       <>  
+                      
                     {products.length ?  <span className='hidden'></span> : <div className='flex justify-center items-center flex-col min-h-80 w-full'>
                       <span className='text-2xl font-Roboto font-medium'>No Products</span>
                       </div>}
                   {products.map((product,index) => (
+                     <>
                     <div className='flex flex-row justify-between  items-center w-full' key={`${product.id}-${index}`}>
-                      <div>
+                      <div className='w-40 h-40 flex flex-col justify-center items-center'>
                       <img src={product.image_url} className=' object-cover w-full h-20 md:h-40' alt="Product" />
                     </div>
                     <div className='flex flex-row w-full justify-between items-center '>
-                      <div className=' mr-2'>
+                      <div className=' mr-2 w-32 md:w-52'>
                         <span className='text-base md:text-xl font-Roboto font-normal'>{product.name}</span>
                       </div>
-                      <div className='text-base mr-2 md:text-lg font-Roboto font-medium flex justify-center items-center border border-gray-300 w-10'>
+                      <div className='text-base mr-2 md:text-lg  font-Roboto font-medium flex justify-center items-center border border-gray-300 w-10'>
                         <span>{product.quantity}</span>
                       </div>
-                      <div>
+                      <div className=' w-16'>
                         <span className='text-base md:text-xl font-Roboto font-medium'>${parseFloat(product.price * product.quantity).toFixed(2)}</span>
                       </div>
                       <div className='flex flex-row justify-between items-center ml-2 w-24 lg:w-40  mr-3 lg:mr-10
@@ -72,12 +80,16 @@ const Cart = () => {
                           <button className='text-base md:text-xl' onClick={()=>deleteEntireProduct(product.id)} type='button'><FontAwesomeIcon icon={faTrash}/></button>
                           </div>                 
                       </div>
-                        
+                      
                     </div>
+                    
                   </div>
-                  
+                  <hr className='w-11/12 ml-6 md:ml-12 border border-gray-300'/>
+                  </>
                 ))}
+                  
                 </>
+                
         </div> 
            <div className='  bg-white min-h-96 w-11/12 md:w-72 border rounded
               border-gray-300 '>
